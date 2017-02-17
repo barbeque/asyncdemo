2b6317ed82d6faae1b3c69044b6aa07a3c95f6cf
mike stedman <github@ravuya.com>
2017-02-17 15:29:56 -0700

    Really tiny async demo

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Threading;
 
 namespace AsyncMess
 {
@@ -23,6 +15,37 @@ namespace AsyncMess
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async Task DoWork(TextBox target, int n)
+        {
+            await Dispatcher.BeginInvoke((Action)(() =>
+             {
+                 target.Text = "Working...";
+             }));
+
+            for (var i = 0; i < n; ++i)
+            {
+                Thread.Sleep(10);
+            }
+
+            await Dispatcher.BeginInvoke((Action)(() =>
+            {
+                target.Text = n.ToString();
+            }));
+        }
+
+        private void KickOffCalc()
+        {
+            var _1 = DoWork(_fib1000, 1000);
+            var _2 = DoWork(_fib100, 100);
+            var _3 = DoWork(_fib10, 10);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Kick it into the background so the button handler doesn't block.
+            Task.Run(() => KickOffCalc());
         }
     }
 }
